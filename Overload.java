eccf73e9929fe7175b630016fd8876c17a8dcc81
Prathamsj12 <132184969+Prathamsj12@users.noreply.github.com>
2024-05-01 11:59:33 +0530

    overloading Method  is Loaded
    Added

---
+++ Overload.java
@@ -0,0 +1,13 @@
+class Overload{
+void v1(){
+System.out.println("This is method One");
+	}
+void v1(int a, int b){
+ System.out.println("This is Method Two---->>>"+(a+b));
+	}
+int v1(int a){
+return a;
+	}
+
+}
+	
